718c0eef43fe55eae9b10ab74d4dfb216155b7ba
Bunma93 <bunma93@gmail.com>
2024-06-29 10:46:08 +0700

    Js-condition13

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // write code here
+let number = Number(prompt("กรอกตัวเลข"));
+switch (number) {
+    case 1:
+        alert("One");
+        break;
+    case 0:
+        alert("Zero");
+        break;
+    case -1:
+        alert("Minus");
+        break;
+    default:
+        alert("Invalid number");
+};
